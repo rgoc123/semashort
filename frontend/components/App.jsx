0a0c3e7784616b60a6787c6501bf4a069a6edfa0
Robert O'Connor <robertgoconnor2@gmail.com>
2018-02-26 19:36:32 -0500

    Finish setup with App and root components

---
+++ frontend/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+//Import components
+
+const App = () => (
+  <div>
+
+    <h1>SemaShort</h1>
+
+  </div>
+);
+
+export default App;
--- frontend/semashort.jsx
+++ frontend/semashort.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import configureStore from './store/store';
-// import Root from './components/root';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   window.$ = $;
@@ -22,5 +22,5 @@ document.addEventListener('DOMContentLoaded', () => {
     window.dispatch = store.dispatch;
   }
 
-  ReactDOM.render(<h1>Welcome to SemaShort</h1>, root);
+  ReactDOM.render(<Root store={ store } />, root);
 });
