35ff01629ef59eb4a77b7ac82a802a25d79c329b
Robert O'Connor <robertgoconnor2@gmail.com>
2018-02-28 19:56:34 -0500

    Maybe fix reroute issue

--- frontend/components/links/link_show.jsx
+++ frontend/components/links/link_show.jsx
@@ -6,25 +6,31 @@ class LinkShow extends React.Component {
 
   constructor(props) {
     super(props);
-    this.shortLink = "https://semashort.herokuapp.com/#/links/" + this.props.match.params.shortLink;
+    this.shortLink = this.props.match.params.shortLink;
     this.link = this.props.link;
     this.link2 = this.props.link2;
   }
 
   componentDidMount() {
-    this.props.fetchLink(this.props.match.params.shortLink);
+    this.props.fetchLink(this.shortLink);
   }
 
   render() {
+    debugger
     if(this.link) {
       window.location = this.link.long_link;
     } else {
       if (!this.props.link2) {
+        debugger
         return (
           null
         );
       } else {
-        let longLink = "https://semashort.herokuapp.com/#/links/" + this.props.match.params.shortLink;
+        debugger
+        let newShortLink = "https://semashort.herokuapp.com/#/links/" + this.shortLink;
+        debugger
+        let longLink = this.props.link2[newShortLink].long_link;
+        debugger
         window.location = longLink;
       }
     }
