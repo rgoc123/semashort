35b5d6b5d0b88f9d20a52479aef1ca12340e26f7
Robert O'Connor <robertgoconnor2@gmail.com>
2018-03-05 19:04:48 -0500

    Adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/api/links/:short_link', :to => 'api/links#show', defaults: {format: :json}
 
   namespace :api, defaults: {format: :json} do
-    resources :links, only: [:index, :create, :show, :redirect]
+    resources :links, only: [:index, :create, :show]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
